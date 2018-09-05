049340a42f9a477b4caac32a6d1a333826111740
MrRightli <>
2018-09-05 16:40:28 +0800

    rm zk

--- routers/web.go
+++ routers/web.go
@@ -5,9 +5,6 @@ import (
 	"xapi/app/controllers"
 	"os"
 	"io"
-	"github.com/samuel/go-zookeeper/zk"
-	"time"
-	"fmt"
 	"xapi/tools"
 )
 
@@ -27,31 +24,6 @@ func InitRouter() *gin.Engine {
 		logger := new(tools.Logger)
 		logger.InitLogger()
 		logger.Info("hello logger-info")
-
-		var hosts = []string{"127.0.0.1:2181"} //server端host
-		conn, _, err := zk.Connect(hosts, time.Second*5)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		var path = "/xapi"
-		var data = []byte("hello zk")
-		var flags = 0
-		//flags有4种取值：
-		//0:永久，除非手动删除
-		//zk.FlagEphemeral = 1:短暂，session断开则改节点也被删除
-		//zk.FlagSequence  = 2:会自动在节点后面添加序号
-		//3:Ephemeral和Sequence，即，短暂且自动添加序号
-		var acls = zk.WorldACL(zk.PermAll) //控制访问权限模式
-
-		p, err_create := conn.Create(path, data, int32(flags), acls)
-		if err_create != nil {
-			fmt.Println(err_create)
-			return
-		}
-		logger.Info("hello logger-info" + p)
-
-		defer conn.Close()
 	})
 
 	admin := router.Group("/admin")
